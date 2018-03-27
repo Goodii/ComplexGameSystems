dd8bbd2d59b43193ba08a2057995c25a6e95b460
Patrick Goodison <iPatt98@gmail.com>
2018-03-27 16:36:59 +1100

    no message

---
+++ aieBootstrap-master/Server/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+#include <RakPeerInterface.h>
+#include <MessageIdentifiers.h>
+#include <BitStream.h>
+
+int main()
+{
+	const unsigned short PORT = 5456;
+	RakNet::RakPeerInterface* pPeerInterface = nullptr;
+
+	//startup the server, and stat it listening to clients
+	std::cout << "Starting up the server...\n";
+
+	//initialise the raknet peer interface first
+	pPeerInterface = RakNet::RakPeerInterface::GetInstance();
+
+	//create a socket descriptor to describe this connection
+	RakNet::SocketDescriptor sd(PORT, 0);
+
+	//now call startup - max of 32 connections, on the assinged port
+	pPeerInterface->Startup(32, &sd, 1);
+	pPeerInterface->SetMaximumIncomingConnections(32);
+
+	handleNetworkMessages(pPeerInterface);
+
+	return 0;
+}
+
+void handleNetworkMessages(RakNet::RakPeerInterface* pPeerInterface)
+{
+	RakNet::Packet* packet = nullptr;
+
+	while (true)
+	{
+		for (packet = pPeerInterface->Receive(); packet;
+			pPeerInterface->DeallocatePacket(packet),
+			packet = pPeerInterface->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_NEW_INCOMING_CONNECTION:
+				std::cout << "A new connection is incoming.\n";
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				std::cout << "A client has disconnected.\n";
+				break;
+			case ID_CONNECTION_LOST:
+				std::cout << "A client has lost connection.\n";
+				break;
+			default:
+				std::cout << "Received a message with an unkown id: " << packet->data[0];
+				break;
+			}
+		}
+	}
+}
---
+++ aieBootstrap-master/ThreadingTutorial/Tutorials.cpp
@@ -0,0 +1,64 @@
+#include "Tutorials.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+#include <thread>
+
+Tutorials::Tutorials() {
+
+}
+
+Tutorials::~Tutorials() {
+
+}
+
+bool Tutorials::startup() {
+	
+	m_2dRenderer = new aie::Renderer2D();
+
+	// TODO: remember to change this when redistributing a build!
+	// the following path would be used instead: "./font/consolas.ttf"
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+
+
+	//lambda
+	using fnc = int(*)(int a, int b);
+
+	auto lambda = [/*encapsulator*/](/*parameters*/) {/*function code*/};
+	
+
+	return true;
+}
+
+void Tutorials::shutdown() {
+
+	delete m_font;
+	delete m_2dRenderer;
+}
+
+void Tutorials::update(float deltaTime) {
+
+	// input example
+	aie::Input* input = aie::Input::getInstance();
+
+	// exit the application
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
+}
+
+void Tutorials::draw() {
+
+	// wipe the screen to the background colour
+	clearScreen();
+
+	// begin drawing sprites
+	m_2dRenderer->begin();
+
+	// draw your stuff here!
+	
+	// output some text, uses the last used colour
+	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
+
+	// done drawing sprites
+	m_2dRenderer->end();
+}
